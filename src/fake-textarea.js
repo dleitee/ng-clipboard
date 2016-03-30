fdc62cc92f22c8a9efe468ab78ed926ede4e6057
Daniel Leite de Oliveira <dleitee@gmail.com>
2016-03-30 13:51:03 -0300

    Add fake-textarea factory.

---
+++ src/fake-textarea.js
@@ -0,0 +1,20 @@
+angular.module("ng-clipboard")
+	.factory("createFake", () => {
+		return (text, command, onSuccess, onError) => {
+			let placeholder = document.createElement("textarea");
+		    placeholder.setAttribute(
+		        "style", 
+		        "position: absolute;overflow: hidden;width: 0;height: 0;top: 0;left: 0;"
+		    );
+		    placeholder.innerText = text;
+		    document.body.appendChild(placeholder);
+		    placeholder.select();
+		    try {
+		        document.execCommand(command);
+		    } catch (err) {
+		        onError(err);
+		    }
+		    onSuccess();
+		    placeholder.remove();
+		};
+	});
