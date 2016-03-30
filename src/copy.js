d0b02ac5437763b55235789bcd2b31cb20f9f416
Daniel Leite de Oliveira <dleitee@gmail.com>
2016-03-30 09:34:08 -0300

    Writing code in es6 format.

--- src/copy.js
+++ src/copy.js
@@ -1,13 +1,17 @@
 angular.module("ng-clipboard")
-    .directive("ngCopy", function() {
+    .directive("ngCopy", () => {
         return {
             restrict: "A",
             scope: {
                 ngCopy: "="
             },
-            link: function(scope, element) {
-                element.on("click", function() {
-                    var placeholder = document.createElement("textarea");
+            link: (scope, element) => {
+                element.on("click", () => {
+                    let placeholder = document.createElement("textarea");
+                    placeholder.setAttribute(
+                        "style", 
+                        "position: absolute;overflow: hidden;width: 0;height: 0;top: 0;left: 0;"
+                    );
                     placeholder.innerText = scope.ngCopy;
                     document.body.appendChild(placeholder);
                     placeholder.select();
